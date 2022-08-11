bc40b8c554c28916f747ae33af87af22f887f95f
liftcat <liftcat@163.com>
2022-08-11 18:53:20 +0800

    home页面的轮播图及轮播图跳转完成

--- main.js
+++ main.js
@@ -6,24 +6,24 @@ import App from './App'
 // 按需导入 $http 对象
 import { $http } from '@escook/request-miniprogram'
 
-// 将按需导入的 $http 挂载到 wx 顶级对象之上，方便全局调用
-wx.$http = $http
-
 // 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
 uni.$http = $http
 
 // 配置请求根路径
-$http.baseUrl = "https://www.uinav.com"
+// $http.baseUrl = "https://www.uinav.com"
+// https://www.showdoc.com.cn/128719739414963/2513235043485226 接口文档
+// 新接口地址：
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
 
-// 请求拦截器，请求开始之前做一些事情
+// 请求开始之前做一些事情
 $http.beforeRequest = function (options) {
 	uni.showLoading({
-		title: "数据加载中"
+		title: '数据加载中...',
 	})
 }
 
-// 响应拦截器
-$http.afterRequest = function() {
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
 	uni.hideLoading()
 }
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,20 @@
 <template>
 	<view>
-		home
+		<!-- 渲染轮播图 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<!-- 循环渲染轮播图的item项 -->
+			<swiper-item v-for="(item, i) in swiperList" :key="i">
+				<!-- <view class="swiper-item">
+					动态绑定图片的 src 属性
+					<image :src="item.image_src" mode=""></image>
+				</view> -->
+				<!-- navigator : 导航标签 -->
+				<navigator class="swiper-item" :url="'/subpackages/goods_detail/goods_detail?goods_id=' + item.goods_id">
+					<!-- 动态绑定图片的 src 属性 -->
+					<image :src="item.image_src" mode=""></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +22,45 @@
 	export default {
 		data() {
 			return {
-				
+				// 存储获取的轮播图数据
+				swiperList: []
 			};
+		},
+		onLoad() {
+			// 调用方法，获取轮播图数据
+			this.getSwiperList()
+		},
+		methods: {
+			async getSwiperList() {
+				// '/' 根目录既为 main.js 配置的 basUrl
+				// 发起请求
+				const { data: res } = await uni.$http.get("/api/public/v1/home/swiperdata");
+				
+				// 请求失败,弹出错误提示
+				if (res.meta.status !== 200) {
+				    return uni.showToast({
+						title: '数据请求失败！',
+				        duration: 1500,
+				        icon: 'none',
+				    })
+				}
+				
+				// 请求成功，为 data 中的 swiperList 赋值
+				console.log(res)
+				this.swiperList = res.message;
+			}
 		}
+		
 	}
 </script>
 
 <style lang="scss">
-
+	swiper {
+		height: 330rpx;
+		
+		.swiper-item,image {
+			width: 100%;
+			height: 100%;
+		}
+	}
 </style>
