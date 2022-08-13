b720c157e1b5abb5fb50b67ec867ad642d62f5c9
liftcat <liftcat@163.com>
2022-08-13 22:36:00 +0800

    goods-list页面完成

--- subpackages/goods_list/goods_list.vue
+++ subpackages/goods_list/goods_list.vue
@@ -1,9 +1,13 @@
 <template>
 	<view>
 		<view class="goods-list">
-			<block v-for="(item, i) in goodsList" :key="i">
+			<view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
 				<my-goods :goods="item"></my-goods>
-			</block>
+			</view>
+			
+			<view class="toTop" v-if="toTop" @click="gotoTop">
+				<uni-icons type="top" size="30"></uni-icons>
+			</view>
 		</view>
 	</view>
 </template>
@@ -26,11 +30,15 @@
 				// 存储商品列表数据
 				goodsList: [],
 				// 总页数
-				tatal: 0
+				total: 0,
+				// 数据节流阀控制器
+				isLoading: false,
+				// 返回顶部
+				toTop: false
 			};
 		},
 		onLoad(options) {
-			console.log(options)
+			// console.log(options)
 			// 每次接受的仅仅是一条参数数据，要么是 ?query 要么是 ?cid
 			this.queryObj.query = options.query || '';
 			this.queryObj.cat_id = options.cid || '';
@@ -39,14 +47,70 @@
 		},
 		methods: {
 			// 创建连接，获取商品列表数据
-			async getGoodsList() {
+			async getGoodsList(callback) {
+				// 打开节流阀
+				this.isLoading = true;
+				
+				// 发起请求
 				const { data: res } = await uni.$http.get("/api/public/v1/goods/search", this.queryObj)
 				
+				// 关闭节流阀
+				this.isLoading = false;
+				
+				// 数据请求完成，执行回调函数
+				callback && callback();
+				
 				if(res.meta.status !== 200) return uni.$showMsg();
-				console.log(res);
-				// 数据赋值
-				this.goodsList = res.message.goods;
-				this.tatal = res.message.tatal;
+				// console.log(res);
+				// 数据赋值,展开运算符：将 ...res.message.goods 的数据拼接到 ...this.goodsList ,再赋值给 this.goodsList
+				this.goodsList = [...this.goodsList, ...res.message.goods];
+				this.total = res.message.total;
+			},
+			// 跳转到商品详情页面
+			gotoDetail(item) {
+				uni.navigateTo({
+					url: "/subpackages/goods_detail/goods_detail?goods_id=" + item.goods_id
+				})
+			},
+			// 返回页面顶部
+			gotoTop() {
+				uni.pageScrollTo({
+					scrollTop: 0,
+					duration: 300
+				})
+			}
+		},
+		// 监听页面触底事件
+		onReachBottom() {
+			// 判断数据是否加载完成：当前页码 * 每页的数据条数
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg("数据加载完成！");
+			
+			// this.isLoading 为 true 既代表正在请求数据（执行 getGoodsList() 函数）
+			if(this.isLoading) return
+			
+			// 页码增加1
+			this.queryObj.pagenum += 1;
+			// 重新获取数据
+			this.getGoodsList()
+		},
+		// 监听页面下拉事件
+		onPullDownRefresh() {
+			// 重置数据
+			this.queryObj.pagenum = 1;
+			this.total = 0;
+			this.goodsList = [];
+			this.isLoading = false;
+			
+			// 发起请求
+			this.getGoodsList(() => {
+				uni.stopPullDownRefresh();
+			})
+		},
+		onPageScroll(e) {
+			if(e.scrollTop < 500) {
+				this.toTop = false;
+			}else {
+				this.toTop = true;
 			}
 		}
 	}
@@ -54,6 +118,18 @@
 
 <style lang="scss">
 	.goods-list {
-		
+		.toTop {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			position: fixed;
+			right: 5%;
+			bottom: 5%;
+			border: 1px solid #f0f0f0;
+			width: 40px;
+			height: 40px;
+			background-color: #ffffff;
+			border-radius: 50%;
+		}
 	}
 </style>
