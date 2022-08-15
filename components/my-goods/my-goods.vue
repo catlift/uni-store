1f7d3876e864e4e8a21dbb19f44a0da6a2ed2354
liftcat <liftcat@163.com>
2022-08-15 16:37:08 +0800

    cart 数据渲染

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,8 +2,11 @@
 	<view>
 		<!-- goods 是从父组件传过来的数据，在 props 接受了 -->
 		<view class="goods-item">
-			<!-- 商品在左侧的图片 -->
+			<!-- 商品左侧的信息（图片） -->
 			<view class="left-goods-item">
+				<!-- 商品左侧的单选按钮, checked 存储在购物车中的商品，包含 goods_state 属性，表示商品的勾选状态  -->
+				<radio checked="checked" color="#D81E06" :checked="goods.goods_state" @click="radioClickHandler" v-if="showRadio"></radio>
+				<!-- 商品在左侧的图片 -->
 				<image :src="goods.goods_small_logo || defaultPicture" class="goods-picture"></image>
 			</view>
 			<!-- 商品在右侧的描述 -->
@@ -37,6 +40,12 @@
 			goods: {
 				type: Object,
 				default: {}
+			},
+			// 是否展示图片左侧的 radio
+			showRadio: {
+				type: Boolean,
+				// 如果外界没有指定 show-radio 属性的值，则默认不展示 radio 组件
+				default: false
 			}
 		},
 		// 过滤器
@@ -44,6 +53,17 @@
 			// 把价格的数字处理为带两位小数的数字
 			toFixed(num) {
 				return Number(num).toFixed(2);
+			}
+		},
+		methods: {
+			// radio 组件的点击事件处理函数
+			radioClickHandler() {
+				this.$emit('radio-change', {
+					// 商品id
+					goods_id: this.goods.goods_id,
+					// 商品最新勾选状态
+					goods_state: !this.goods.goods_state
+				})
 			}
 		}
 	}
@@ -56,6 +76,9 @@
 		border-bottom: 1px solid #f0f0f0;
 		
 		.left-goods-item {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
 			margin-right: 5px;
 			
 			.goods-picture {
--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,12 +1,31 @@
 <template>
 	<view>
-		cart
+		<!-- 购物车商品列表的标题区域 -->
+		<view class="cart-title">
+			<!-- 左侧图标 -->
+			<uni-icons type="shop" size="18"></uni-icons>
+			<!-- 描述文本 -->
+			<text class="cart-title-text">购物车</text>
+		</view>
+		
+		<!-- 渲染加入购物车的商品 -->
+		<block v-for="(goods, i) in cart" :key="i">
+			<!-- 
+				* :goods 传递的是商品对象信息
+				* :show-radio 传递是复选框是否显示
+				*  在 radioChangeHandler 事件处理函数中，通过事件对象 e，得到商品的 goods_id 和 goods_state
+			 -->
+			<my-goods :goods="goods" :show-radio="true" @radio-change="radioChangeHandler"></my-goods>
+		</block>
+		
 	</view>
 </template>
 
 <script>
 	// 导入封装的 mixin 模块
 	import badgeMixin from "@/mixins/tabbar-badge.js"
+	// 导入 cart 的 store
+	import { mapState } from "vuex"
 	
 	export default {
 		// 将 badgeMixin 混入到当前页面进行使用
@@ -15,10 +34,34 @@
 			return {
 				
 			};
+		},
+		onLoad() {
+			console.log(this.cart)
+		},
+		computed: {
+			// 将 m_cart 模块中的 cart 数组 映射到当前页面
+			...mapState('m_cart', ['cart']),
+		},
+		methods: {
+			radioChangeHandler(e) {
+				console.log(e)
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	.cart-title {
+		display: flex;
+		// justify-content: space-between;
+		align-items: center;
+		padding-left: 5px;
+		border-bottom: 1px solid #efefef;
+		font-size: 14px;
+		height: 40px;
+		
+		.cart-title-text {
+			margin-left: 10px;
+		}
+	}
 </style>
